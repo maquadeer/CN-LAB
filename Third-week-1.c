6d1c1e196c406a22a8e5ddb943bae56458cdad9e
MOHAMMAD ABDUL QUADEER <136174168+maquadeer@users.noreply.github.com>
2023-11-20 14:34:47 +0530

    Update Third-week-1.c

--- Third-week-1.c
+++ Third-week-1.c
@@ -1,37 +1,36 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h>
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#define PORT 8080
-#define MAXLINE 1024 // Driver code
-int main() {
-  int sockfd;
-  char buffer[MAXLINE];
-  char * hello = "Hello from server";
-  struct sockaddr_in servaddr, cliaddr; // Creating socket file descriptor 
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("socket creation failed");
-    exit(EXIT_FAILURE);
-  }
-  memset( & servaddr, 0, sizeof(servaddr));
-  memset( & cliaddr, 0, sizeof(cliaddr)); // Filling server information 
-  servaddr.sin_family = AF_INET; // IPv4 
-  servaddr.sin_addr.s_addr = INADDR_ANY;
-  servaddr.sin_port = htons(PORT); // Bind the socket with the server address 
-  if (bind(sockfd, (const struct sockaddr * ) & servaddr, sizeof(servaddr)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
-  int len, n;
-  len = sizeof(cliaddr); //len is value/result 
-  n = recvfrom(sockfd, (char * ) buffer, MAXLINE, MSG_WAITALL, (struct sockaddr * ) & cliaddr, & len)
-  buffer[n] = '\0';
-  printf("Client : %s\n", buffer);
-  sendto(sockfd, (const char * ) hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr * ) & cliaddr, len);
-  printf("Hello message sent.\n");
-  return 0;
-}
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<sys/socket.h>
+
+#include<netinet/in.h>
+
+#include<sys/types.h>
+
+#include<string.h>
+
+main(int argc, char * argv[]) {
+    struct sockaddr_in s;
+    int rval, sockid, slen;
+    char m1[20], m2[20];
+    system("clear");
+    if (argc < 3) {
+      printf("\nUSAGE : %s IP-Address Port#\n", argv[0]);
+      exit(1);
+    }
+    sockid = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockid == -1) {
+      perror("SOCK-CRE-ERR:");
+      exit(1);
+    }
+    s.sin_family = AF_INET;
+    s.sin_addr.s_addr = inet_addr(argv[1]);
+    s.sin_port = htons(atoi(argv[2]));
+    printf("\nEnter the request message : ");
+    scanf("%s", m1);
+    slen = sizeof(s);
+    rval = sendto(sockid, m1, sizeof(m1), 0, (struct sockaddr * ) & s, slen);
+    if (rval == -1) {
+      perror("MSG-SEND-ERR:");
+      exit(1);
